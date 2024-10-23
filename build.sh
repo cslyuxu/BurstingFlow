75645adc54b139d0b80a232cad4ee3abed5f05ef
Xu Lyu <cslyuxu@comp-dept-ad2.comp.hkbu.edu.hk>
2024-10-23 16:24:59 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #-lpthread
-g++ ./cpp/main.cpp -std=c++17 -O3 -o MAPSE
+g++ ./cpp/main.cpp -std=c++17 -O3 -o BFQ
 
 ulimit -s unlimited
 
@@ -7,69 +7,69 @@ ulimit -s unlimited
 
 
 #### k =0: Query Generation
-#./MAPSE query/ctu network/ctu 0
+#./BFQ query/ctu network/ctu 0
 
-#./MAPSE query/prosper network/prosper 0
+#./BFQ query/prosper network/prosper 0
 
-#./MAPSE query/btc_2011 network/btc_2011 0
+#./BFQ query/btc_2011 network/btc_2011 0
 
-#./MAPSE query/dot network/dot 0
+#./BFQ query/dot network/dot 0
 
-#./MAPSE query/terraforms network/terraforms 0
+#./BFQ query/terraforms network/terraforms 0
 
-#./MAPSE query/bayc network/bayc 0
+#./BFQ query/bayc network/bayc 0
 
-#./MAPSE query/meebits network/meebits 0
+#./BFQ query/meebits network/meebits 0
 
-#./MAPSE query/artblock network/artblock 0
+#./BFQ query/artblock network/artblock 0
 
 
 
 
 ##### k = 1: Overall                   
-#./MAPSE query/ctu network/ctu 1
+#./BFQ query/ctu network/ctu 1
 
-#./MAPSE query/prosper network/prosper 1
+#./BFQ query/prosper network/prosper 1
 
-#./MAPSE query/btc_2011 network/btc_2011 1
+#./BFQ query/btc_2011 network/btc_2011 1
 
-./MAPSE query/bayc network/bayc 1
+./BFQ query/bayc network/bayc 1
 
 
 ##### k = 2: Varying Delta 
 
 
-#./MAPSE query/ctu network/ctu 2
+#./BFQ query/ctu network/ctu 2
 
-#./MAPSE query/prosper network/prosper 2
+#./BFQ query/prosper network/prosper 2
 
-#./MAPSE query/btc_2011 network/btc_2011 2
+#./BFQ query/btc_2011 network/btc_2011 2
 
-#./MAPSE query/bayc network/bayc 2
+#./BFQ query/bayc network/bayc 2
 
 
 ##### k = 3: Investigating Num of Incremental Cases
 
-#./MAPSE query/ctu network/ctu 3
+#./BFQ query/ctu network/ctu 3
 
-#./MAPSE query/prosper network/prosper 3
+#./BFQ query/prosper network/prosper 3
 
-#./MAPSE query/bayc network/bayc 3
+#./BFQ query/bayc network/bayc 3
 
 
 ##### k = 4: TransTime                   
-#./MAPSE query/ctu network/ctu 4
+#./BFQ query/ctu network/ctu 4
 
-#./MAPSE query/prosper network/prosper 4
+#./BFQ query/prosper network/prosper 4
 
-#./MAPSE query/btc_2011 network/btc_2011 4
+#./BFQ query/btc_2011 network/btc_2011 4
 
-#./MAPSE query/bayc network/bayc 4
+#./BFQ query/bayc network/bayc 4
 
 
 ##### k = 5: Varying Size                   
-#./MAPSE query/ctu network/ctu 5
+#./BFQ query/ctu network/ctu 5
 
-#./MAPSE query/prosper network/prosper 5
+#./BFQ query/prosper network/prosper 5
 
-#./MAPSE query/btc_2011 network/btc_2011 5
+#./BFQ query/btc_2011 network/btc_2011 5
